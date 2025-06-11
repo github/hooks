e7b996c4ff44cd91fdd8369ce4c9239f43f658e3
GrantBirki <grant.birkinbine@gmail.com>
2025-06-11 09:01:46 -0700

    feat: add custom auth plugin and related tests

--- spec/acceptance/plugins/handlers/test_handler.rb
+++ spec/acceptance/plugins/handlers/test_handler.rb
@@ -4,6 +4,7 @@ class TestHandler < Hooks::Plugins::Handlers::Base
   def call(payload:, headers:, config:)
     {
       status: "test_success",
+      handler: "TestHandler",
       payload_received: payload,
       config_opts: config[:opts],
       timestamp: Time.now.iso8601
