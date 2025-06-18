e49431a112e786d7c4688af8ed09462d8453366b
GrantBirki <grant.birkinbine@gmail.com>
2025-06-18 13:53:09 -0700

    bump

--- lib/hooks/version.rb
+++ lib/hooks/version.rb
@@ -4,5 +4,5 @@
 module Hooks
   # Current version of the Hooks webhook framework
   # @return [String] The version string following semantic versioning
-  VERSION = "0.5.0".freeze
+  VERSION = "0.5.1".freeze
 end
