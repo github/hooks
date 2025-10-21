3ce6fffea5fddf629b4791a8ca967883531889c3
GrantBirki <grant.birkinbine@gmail.com>
2025-10-21 09:38:32 -0700

    remove vendored sigstore cli

--- vendor/cache/sigstore-ruby-ce93acf7fa7e/test/sigstore/internal/merkle_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class Sigstore::Internal::MerkleTest < Test::Unit::TestCase
-  test "verify_merkle_inclusion" do
-    nil
-  end
-end
--- vendor/cache/sigstore-ruby-ce93acf7fa7e/test/sigstore/rekor/client_test.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class Sigstore::Rekor::ClientTest < Test::Unit::TestCase
-end
